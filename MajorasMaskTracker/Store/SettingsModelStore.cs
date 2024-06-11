562371cf1d1e440539a8e032c306215fd6b145d2
INTRANET\marlon.schmitz <Marlon.Schmitz@autoelectric.com>
2024-06-11 14:58:46 +0200

    Did some stuff

--- MajorasMaskTracker/Store/SettingsModelStore.cs
+++ MajorasMaskTracker/Store/SettingsModelStore.cs
@@ -1,8 +1,21 @@
 ﻿using BaseClasses;
+using MajorasMaskTracker.Model;
 
 namespace MajorasMaskTracker.Store;
 
 public class SettingsModelStore : BaseModel
 {
+    private SettingsModel _settingsModel = new();
+    public SettingsModel SettingsModel
+    {
+        get => _settingsModel;
+        set
+        {
+            _settingsModel = value;
+            NotifyPropertyChanged();
+        }
+    }
     
+    
+    public static SettingsModelStore Instance { get; } = new();
 }
